fe8bc363f2b00bc7b9c1c0ba1e7c09c2ee14a06e
Martín Violi <martinvioli29@gmail.com>
2022-04-06 15:21:52 -0300

    asd

--- Ejercicios/05-MinStack/MinStack.js
+++ Ejercicios/05-MinStack/MinStack.js
@@ -1,14 +1,102 @@
 class Node {
   // Your code here:
-
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-class MinStack {
-  // Your code here:
+class Stack {
+  constructor(value) {
+    this.top = null;
+  }
 
+  push(value) {
+    if (!this.top) {
+      this.top = new Node(value);
+    } else {
+      const oldTop = this.top;
+      this.top = new Node(value);
+      this.top.next = oldTop;
+      return this.top && this.top.value;
+    }
+  }
+
+  pop() {
+    if (!this.top) {
+      return "The stack is now empty.";
+    } else {
+      const newTop = this.top.next;
+      const oldTop = this.top;
+      this.top = newTop;
+      return oldTop && oldTop.value;
+    }
+  }
+
+  peek() {
+    if (!this.top) {
+      return "The stack is empty.";
+    } else {
+      return this.top && this.top.value;
+    }
+  }
+}
+
+class MinStack extends Stack {
+  // Your code here:
+  constructor(value) {
+    super();
+    this.minimum = new Stack();
+  }
+
+  push(value) {
+    if (!this.top) {
+      this.top = new Node(value);
+      this.minimum.push(value);
+      return this.top && this.top.value;
+    } else {
+      const oldTop = this.top;
+      this.top = new Node(value);
+      this.top.next = oldTop;
+      if (this.minimum.peek() < this.top.value) {
+        this.minimum.push(this.minimum.peek());
+      } else {
+        this.minimum.push(value);
+      }
+      return this.top && this.top.value;
+    }
+  }
+
+  pop() {
+    if (!this.top) {
+      return "The stack is empty.";
+    } else {
+      const newTop = this.top.next;
+      const oldTop = this.top;
+      this.top = newTop;
+      this.minimum.pop();
+      return oldTop && oldTop.value;
+    }
+  }
+
+  peek() {
+    if (!this.top) {
+      return "The stack is empty.";
+    } else {
+      return this.top && this.top.value;
+    }
+  }
+
+  min() {
+    if (!this.top) {
+      return "The stack is empty.";
+    } else {
+      return this.minimum.peek();
+    }
+  }
 }
 
 module.exports = {
   Node,
-  MinStack
-}
+  MinStack,
+};
--- Ejercicios/12-Intersection/Intersection.js
+++ Ejercicios/12-Intersection/Intersection.js
@@ -1,6 +1,13 @@
 function intersection(arr1, arr2) {
   // Your code here:
-
+  var joinedArr = [...arr1, ...arr2];
+  var copys = [];
+  for (var i = 1; i < joinedArr.length; i++) {
+    if (joinedArr.slice(i).some((e) => e === joinedArr[i - 1])) {
+      copys.push(joinedArr[i - 1]);
+    }
+  }
+  return Array.from(new Set(copys));
 }
 
 module.exports = intersection;
